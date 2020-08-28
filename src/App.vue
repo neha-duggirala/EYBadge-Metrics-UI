f03d57b3b6e2e7221c0da658c3ba54c98d098c5f
neha-d <nduggirala@cardlytic.com>
2020-08-29 02:25:48 +0530

    UPDATED nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,26 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-btn to="/">
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <span class="mr-2">Home</span>
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-<v-content class="ats-container">
-          <router-view></router-view>
-        </v-content>
-    <v-main>
-    </v-main>
+    <v-content class="ats-container">
+      <router-view></router-view>
+    </v-content>
+    <v-main></v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  
+  name: "App",
+
   data: () => ({
     //
   }),
