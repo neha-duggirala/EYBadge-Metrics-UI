e8bfbfc0bd338b0e23624efc976c143c5c6e2d51
neha-d <nduggirala@cardlytic.com>
2020-08-28 18:13:10 +0530

    installed axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import AxiosService from './services/common/AxiosService';
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+AxiosService.getConfig().then(()=>{
+  new Vue({
+    render: h => h(App),
+    router,
+    vuetify,
+    store
+  }).$mount('#app');  
+});
+
